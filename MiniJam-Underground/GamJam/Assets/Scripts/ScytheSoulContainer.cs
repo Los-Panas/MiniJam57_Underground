edd0028e87345c645bf3b890dbce58d1e1b5ae46
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-06-28 15:14:51 +0200

    Guadaña FX

---
+++ MiniJam-Underground/GamJam/Assets/Scripts/ScytheSoulContainer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScytheSoulContainer : MonoBehaviour
+{
+    public GameObject SoulContainer;
+    public static bool change_container;
+    public Transform LampContainer;
+    public Transform BaseLamp;
+    public Transform Scythe;
+    public Transform ScytheContainer;
+
+    public GameObject SoulCube;
+    public GameObject LampParticles;
+    public GameObject ScytheParticles;
+
+    public GameObject trail;
+ 
+
+    private void Start()
+    {
+        SoulCube.SetActive(true);
+        LampParticles.SetActive(true);
+        ScytheParticles.SetActive(false);
+        trail.GetComponent<TrailRenderer>().enabled = false;
+    }
+    void Update()
+    {
+        if(change_container)
+        {
+            transform.SetParent(Scythe);
+            transform.localPosition = ScytheContainer.transform.localPosition;
+            SoulCube.SetActive(false);
+            LampParticles.SetActive(false);
+            //ScytheParticles.SetActive(true);
+            trail.GetComponent<TrailRenderer>().enabled = true;
+        }
+        else
+        {
+            transform.SetParent(BaseLamp);
+            transform.localPosition = LampContainer.transform.localPosition;
+            SoulCube.SetActive(true);
+            LampParticles.SetActive(true);
+            ScytheParticles.SetActive(false);
+            trail.GetComponent<TrailRenderer>().enabled = false;
+        }
+    }
+}
