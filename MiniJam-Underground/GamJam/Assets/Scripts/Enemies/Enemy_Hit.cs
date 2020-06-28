2076d2cd1b042c65a17bf84e010a0f1d805d8995
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-06-28 18:14:12 +0200

    Various things

---
+++ MiniJam-Underground/GamJam/Assets/Scripts/Enemies/Enemy_Hit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Hit : MonoBehaviour
+{
+    public int hits_to_kill = 1;
+    public GameObject soul;
+    public void KillEnemy()
+    {
+        // TO DO: Dissolve Shader
+        // TO DO: Particle System
+        Instantiate(soul, transform);
+    }
+}
